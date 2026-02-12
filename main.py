3dcc797b0baa563679824d4725282663b2d308a0
pfgonsitemarketing <pfg.onsite.marketing@gmail.com>
2026-02-12 10:50:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,48 @@ import os
 # Basis-Links
 # ---------------------------------
 basis_links = {
-    "Ulla Popken DE": "https://www.ullapopken.de/de",
+    "Happy Size BEFR": "https://www.happy-size.be/fr",
+    "Happy Size BENL": "https://www.happy-size.be/nl",
+    "Happy Size CHDE": "https://www.happy-size.ch/de",
+    "Happy Size CHFR": "https://www.happy-size.ch/fr",
     "Happy Size DE": "https://www.happy-size.de/de",
+    "Happy Size FR": "https://www.happy-size.fr/fr",
+    "Happy Size NL": "https://www.happy-size.nl/nl",
+
+    "JP1880 BEFR": "https://www.jp1880.be/fr",
+    "JP1880 BENL": "https://www.jp1880.be/nl",
+    "JP1880 CHDE": "https://www.jp1880.ch/de",
+    "JP1880 CHFR": "https://www.jp1880.ch/fr",
     "JP1880 DE": "https://www.jp1880.de/de",
+    "JP1880 FR": "https://www.jp1880.fr/fr",
+    "JP1880 NL": "https://www.jp1880.nl/nl",
+    "JP1880 PL": "https://www.jp1880.pl/pl",
+
+    "Laurason DE": "https://www.laurason.de/de",
+
+    "Studio Untold DE": "https://www.studiountold.de/de",
+
+    "Ulla Popken AT": "https://www.ullapopken.at/de",
+    "Ulla Popken BEFR": "https://www.ullapopken.be/fr",
+    "Ulla Popken BENL": "https://www.ullapopken.be/nl",
+    "Ulla Popken CHDE": "https://www.ullapopken.ch/de",
+    "Ulla Popken CHFR": "https://www.ullapopken.ch/fr",
+    "Ulla Popken CZ": "https://www.ullapopken.cz/cs",
+    "Ulla Popken DE": "https://www.ullapopken.de/de",
+    "Ulla Popken DK": "https://www.ullapopken.dk/da",
+    "Ulla Popken ES": "https://www.ullapopken.es/es",
+    "Ulla Popken EUDE": "https://www.ullapopken.eu/de",
+    "Ulla Popken EUEN": "https://www.ullapopken.eu/en",
+    "Ulla Popken FI": "https://www.ullapopken.fi/fi",
+    "Ulla Popken FR": "https://www.ullapopken.fr/fr",
+    "Ulla Popken IT": "https://www.ullapopken.it/it",
+    "Ulla Popken NL": "https://www.ullapopken.nl/nl",
+    "Ulla Popken NO": "https://www.ullapopken.no/no",
+    "Ulla Popken PL": "https://www.ullapopken.pl/pl",
+    "Ulla Popken RO": "https://www.ullapopken.ro/ro",
+    "Ulla Popken SE": "https://www.ullapopken.se/sv",
+    "Ulla Popken UK": "https://www.ullapopken.co.uk/en",
+    "Ulla Popken US": "https://www.ullapopken.com/en",
 }
 
 # ---------------------------------
